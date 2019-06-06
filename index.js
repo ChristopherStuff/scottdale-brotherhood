6117999f2bc3af67820bce1479e2fbffafee7e7c
Kory McGregor <artemka076.dimaneal@gmail.com>
2019-06-06 21:52:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ connection.on('error', function(err) {
     }
 });
 
-const version = '5.2.0';
+const version = '5.2.1-hide';
 // Первая цифра означает глобальное обновление. (global_systems)
 // Вторая цифра обозначет обновление одной из подсистем. (команда к примеру)
 // Третяя цифра обозначает количество мелких фиксов. (например опечатка)
@@ -499,6 +499,26 @@ async function nalog_biz(){
                         connection.query(`UPDATE \`storage\` SET nalog_new = '${+date + 3600000}' WHERE \`id\` = '${storage.id}'`);
                         let member = bot.guilds.get(storage.server).members.get(storage.owner);
                         send_action(storage.server, `${member.displayName || member.user.tag} (${storage.owner}) c предприятия списан налог (NEED: ${storage.nalog} - NOW: ${storage.money - storage.nalog}). Предприятие - ${storage.name}`);
+                    }
+                }
+            });
+        });
+
+        connection.query(`SELECT * FROM \`buy_dashboard\``, async (error, shops) => {
+            shops.forEach(shop => {
+                let date = new Date().valueOf();
+                if (shop.nalog_new < date){
+                    if (shop.money < shop.nalog){
+                        if (shop.status == true) {
+                            connection.query(`UPDATE \`buy_dashboard\` SET status = '0' WHERE \`id\` = '${shop.id}'`);
+                            let member = bot.guilds.get(shop.server).members.get(shop.owner);
+                            send_action(shop.server, `${member.displayName || member.user.tag} (${shop.owner}) предприятие было закрыто за неуплату налога (NEED: ${shop.nalog} - NOW: ${shop.money}). Предприятие - ${shop.name}`);
+                        }
+                    }else{
+                        connection.query(`UPDATE \`buy_dashboard\` SET money = money - ${shop.nalog} WHERE \`id\` = '${shop.id}'`);
+                        connection.query(`UPDATE \`buy_dashboard\` SET nalog_new = '${+date + 3600000}' WHERE \`id\` = '${shop.id}'`);
+                        let member = bot.guilds.get(shop.server).members.get(shop.owner);
+                        send_action(shop.server, `${member.displayName || member.user.tag} (${shop.owner}) c предприятия списан налог (NEED: ${shop.nalog} - NOW: ${shop.money - shop.nalog}). Предприятие - ${shop.name}`);
                     }
                 }
             });
