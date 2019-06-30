a04a54c3dd5d723901980abb86e130c5dab40ae6
Kory McGregor <artemka076.dimaneal@gmail.com>
2019-07-01 00:30:38 +0300

    Update functions.js

--- objects/functions.js
+++ objects/functions.js
@@ -22,7 +22,7 @@ exports.getDateString = () => {
 
 exports.getDateMySQL = () => {
     let date = new Date();
-    return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}` +
+    return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')} ` +
         `${date.getHours().toString().padStart(2, '0')}:` +
         `${date.getMinutes().toString().padStart(2, '0')}:` +
         `${date.getSeconds().toString().padStart(2, '0')}`;
